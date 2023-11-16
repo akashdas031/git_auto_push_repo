5f3c932a212596b29d99f6c913d246650c81746e
root <root@ip-172-31-34-226.ap-south-1.compute.internal>
2023-11-16 09:48:47 +0000

    again git auto push file modified...

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -12,10 +12,16 @@
    if [ -d .git ]; then 
 	   if [ "git status --porcelain | wc -l " == '0']; then
 		   echo "Everything in this directory is already commited to the local repo...you can go forward to perform the action..."
+		   if [ "git remote | wc -l " == '0' ]; then
+			   echo " No remote repository found....add remote repo to perform action.."
+		   else
+			   echo " action can be performed..."
+			   git remote
+		   fi
 	   else
 		   echo "all files in this repo is not commited to the local repository...commit all files and try again..."
 	   fi
-	   echo "git is initialized and perform auto push operation...  "
+	  
    else
 	   echo "git is not initialized...do you want to initialize to perform operation???  "
    fi
