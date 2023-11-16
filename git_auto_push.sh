cb797395df919cee6e0e17b17fe798945e01bc8c
root <root@ip-172-31-34-226.ap-south-1.compute.internal>
2023-11-16 09:33:38 +0000

    again updated git auto commit script...

--- git_auto_push.sh
+++ git_auto_push.sh
@@ -10,6 +10,11 @@
   
 
    if [ -d .git ]; then 
+	   if [ "git status --porcelain | wc -l " == '0']; then
+		   echo "Everything in this directory is already commited to the local repo...you can go forward to perform the action..."
+	   else
+		   echo "all files in this repo is not commited to the local repository...commit all files and try again..."
+	   fi
 	   echo "git is initialized and perform auto push operation...  "
    else
 	   echo "git is not initialized...do you want to initialize to perform operation???  "
